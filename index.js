57380589b11a42d2f53155d856413c950ba05980
Bharat Nagda <nagdabharat@gmail.com>
2020-06-15 15:11:45 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,25 @@
+function breakOut(array, changeValue, stopValue) {
+  for(let i = 0; i < array.length; i++) {
+    if (array.[i] === stopValue) {
+      break;
+    }
+    array.[i] = changeValue;
+  }
+}
+
+function skipValue(array, changeValue, skipValue) {
+  for(let i = 0; i < array.length; i++) {
+    if (array.[i] === skipValue) {
+      continue;
+    }
+    array.[i] = changeValue;
+  }
+}
+
+function findBy(array, findFn) {
+  for (let i = 0; i < array.length; i++) {
+      findFn(array[i]);
+        
+      }
+  }
+}
