1f076685b4f3154686eb7ac788cd9fcdf155bcb0
Bharat Nagda <nagdabharat@gmail.com>
2020-06-15 15:27:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (var i = 0; i < array.length; i++) {
     if (findFn(array[i])) {
-      return findFn(array[i])
+      return array[i];
     }
   }
   return null;
